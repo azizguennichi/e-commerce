f53efaf96c712d51621e9bfeecf0387e2d4deb2a
azizguennichi <ezzoguennichi@gmail.com>
2022-10-25 14:29:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Product from './components/Product';
+import {Routes,Route} from 'react-router-dom';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+   
+      <Routes>
+<Route  exact path="/Home" element={<Home />} />
+<Route   exact path='/products' element={<Product />} />
+<Route   exact path='/products/:id' element={<ProductDetail />} />
+<Route   exact path='/cart/' element={<Cart/>} />
+<Route   exact path='/checkout/' element={<Checkout/>} />
+<Route  exact path="/about" element={<About />} />
+<Route exact path="/contact" element={<Contact />} />
+
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
